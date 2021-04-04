53535ccfd8e3ee95d228c94d2165c0472386a3a5
Beyonce Knowles <you@example.com>
2021-04-04 18:42:52 -0400

    pg config

---
+++ postgrator-config.js
@@ -0,0 +1,13 @@
+/* eslint-disable quotes */
+require('dotenv').config();
+
+const pg = require('pg');
+pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
+
+module.exports = {
+  'migrationsDirectory': 'migrations',
+  'driver': 'pg',
+  'connectionString': (process.env.NODE_ENV === 'test')
+    ?  process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
+};
