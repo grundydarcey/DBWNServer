d5451ee3fca4dbc6bcf526572be74057ac025c79
Beyonce Knowles <you@example.com>
2021-04-04 19:02:00 -0400

    package fix

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,8 +1,8 @@
 /* eslint-disable quotes */
 require('dotenv').config();
 
-const pg = require('pg');
-pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
+// const pg = require('pg');
+// pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
 
 module.exports = {
   'migrationsDirectory': 'migrations',
